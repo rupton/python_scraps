9d9e8df5844d80fd799207d484caee4189eef9b6
Ryan Upton <ryanjupton@gmail.com>
2026-01-21 18:07:10 -0500

    Making some changes

--- fib.py
+++ fib.py
@@ -1,11 +1,26 @@
+
+from typing import Dict
+
+
+memo: Dict[int, int] = {0: 0, 1: 1}
+#Using memoitization
+def fib3(n: int) -> int:
+    if n not in memo:
+        print(f"Calling fib3({n})")
+        memo[n] = fib3(n - 1) + fib3(n - 2)
+    return memo[n]
+
 def fib(fib_val:int)-> int:
     if fib_val < 2:
         return fib_val
     return fib(fib_val -1) + fib(fib_val -2 )
 
 def fib2(n: int) -> int:
+    print(f"Calling fib2({n}).")
     if n < 2: # base case
          return n
     return fib2(n - 2) + fib2(n - 1) # recursive case
 
-print(fib2(20))
+#print(fib2(20))
+# This would never complete without memoitization
+print(fib3(999))
--- scratch_sort.py
+++ scratch_sort.py
@@ -7,9 +7,7 @@ def sort(nums):
     for i in range(1, len(nums) ):
         for j in range(0, i):
             if nums[i] < nums[j]:
-                print(f"Swapping {nums[i]} and {nums[j]} with indexes {i} and {j}")
                 swap(j, i, nums)
-                print(f"Now I have {nums}")
     return nums
 
 print(sort([23, 45, 13, 12, 1]))
