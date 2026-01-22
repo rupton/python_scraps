6b2c1901d3050bed2dff39b3a8619f6306a90cac
rupton <ryanjupton@gmail.com>
2026-01-22 14:42:46 -0500

    Goofing with formatting.

--- fib.py
+++ fib.py
@@ -1,21 +1,26 @@
 
 from typing import Dict
 
-
+total_calls = 0
 memo: Dict[int, int] = {0: 0, 1: 1}
 #Using memoitization
 def fib3(n: int) -> int:
+    global total_calls 
+    total_calls += 1
     if n not in memo:
-        print(f"Calling fib3({n})")
         memo[n] = fib3(n - 1) + fib3(n - 2)
     return memo[n]
 
 def fib(fib_val:int)-> int:
+    global total_calls 
+    total_calls += 1
     if fib_val < 2:
         return fib_val
     return fib(fib_val -1) + fib(fib_val -2 )
 
 def fib2(n: int) -> int:
+    global total_calls 
+    total_calls += 1
     print(f"Calling fib2({n}).")
     if n < 2: # base case
          return n
@@ -23,4 +28,4 @@ def fib2(n: int) -> int:
 
 #print(fib2(20))
 # This would never complete without memoitization
-print(fib3(999))
+print(f"The 999th number in the Fibonacci Sequence is {fib3(999)} and it took us {total_calls} calls to calculate it using memoitization")
